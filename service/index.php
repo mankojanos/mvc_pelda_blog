6fc878a7aae3142a6653a701b7599ad23889d4ce
Janos <47101526+mankojanos@users.noreply.github.com>
2021-02-09 20:02:18 +0100

    Update index.php

--- service/index.php
+++ service/index.php
@@ -1,1 +1,24 @@
 <?php
+try {
+    require_once __DIR__ .'/Routing.php';
+
+    $fajlok = scandir(__DIR__);
+    foreach ($fajlok as $fajl) {
+        if(preg_match('/.*Rest\\.php/', strltolwer($fajl))) {
+            include_once __DIR__ . '/' .$fajl;
+        }
+    }
+
+    $routing = Routing::getPeldany();
+    $routing->trueCors("*", "origin, content-type, accept, authorization");
+    $routing = $routing->routingRequestFeldolgozasa();
+
+    if(!$routing) {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 400 hiba');
+        die();
+    }
+} catch (Exception $e) {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 500 vegzetes hiba');
+    header('Content-Type: application/json');
+    die(json_encode(array("error" => $e->getMessage())));
+}
