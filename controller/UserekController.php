5160a198203235f912c2c4970bd31515bfe4e3e4
Janos <47101526+mankojanos@users.noreply.github.com>
2021-02-11 19:20:55 +0100

    Update UserekController.php

--- controller/UserekController.php
+++ controller/UserekController.php
@@ -33,9 +33,48 @@ class UserekController extends AlapController {
         } else {
             $errors = array();
             $errors['usernev'] = 'A user nem jo';
-            $this->view->render('userek', 'belep');
+            $this->view->setValtozo('errors', $errors);
         }
     }
-
+    $this->view->render('userek', 'belep');
     }
+
+    /**
+     * Regisztracio
+     * Ha GET request erkezik az oldalt jelenitjuk meg
+     * HA POST request erkezik akkor regisztral az oldalon
+     */
+    public function regisztracio() {
+        $user = new User();
+
+        if(filter_input(INPUT_POST, 'usernev')) {
+            $usernev = filter_input(INPUT_POST, 'usernev', FILTER_SANITIZE_SPECIAL_CHARS);
+            $user->setUsernev($usernev);
+            $user->setPasswd(filter_input(INPUT_POST, 'passwd', FILTER_SANITIZE_SPECIAL_CHARS));
+
+            try {
+                $user->regisztracioEllenorzese();
+                if(!$this->userAbKezelo->usernevLetezik($usernev)) {
+                    $this->userAbKezelo->mentes($user);
+                    $this->view->setMessageSession('A user: ' . $user->getUsernev() . ' sikeresen felveve. Kerem jelenetekezzen be!');
+                    $this->view->atiranyitas('userek', 'belep');
+                } else {
+                    $errors = [];
+                    $errors['usernev'] = 'a felhasznalo mar letezik';
+                  $this->view->setValtozo('errors', $errors );
+                }
+            } catch (ValidacioException $e) {
+                $this->view->setValtozo('errors', $e->getErrors());
+            }
+        }
+
+        $this->view->setValtozo('user', $user);
+        $this->view->render('userek', 'regisztracio');
+    }
+    
+    public function kilep() {
+        session_destroy();
+        $this->view->atiranyitas('userek', 'belep');
+    }
+
 }
